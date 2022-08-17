5e854b8a030800aa4a48f557363c15766be405d0
lishujing <super_lishujing@163.com>
2022-08-17 22:22:58 +0800

    feat: 全局引入字体样式

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import React from 'react';
 // import './App.css'; <div className="app"></div>
 // 使用 JSS 模块化引入组件，将 CSS 文件转换为对象的方式
 import styles from './App.module.css' //  <div className={styles.app}></div>
+import logo from './assets/images/logo.svg'
 import robots from './mock/robots.json'
 import Robot from './components/Robot'
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img className={styles.appLogo} src={logo} alt="logo" />
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+      </div>
       <div className={styles.robotList}>
         {robots.map(r => <Robot id={r.id} email={r.email} name={r.name} />)}
       </div>
