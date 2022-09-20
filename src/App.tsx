c67d5ea5351e6c58eeec85db8aa8e5bd0ed3b8d0
lishujing <super_lishujing@163.com>
2022-09-20 14:40:45 +0800

    feat(assets): 加载媒体与字体文件

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 // 直接引入整个 CSS 文件
 // import './App.css'; <div className="app"></div>
 // 使用 JSS 模块化引入组件，将 CSS 文件转换为对象的方式
+import logo from './assets/images/logo.svg'
 import styles from './App.module.css' //  <div className={styles.app}></div>
 import robots from './mock/robots.json'
 import Robot from './components/Robot'
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+      </div>
       <div className={styles.robotList}>
         {robots.map(r => <Robot id={r.id} email={r.email} name={r.name} />)}
       </div>
