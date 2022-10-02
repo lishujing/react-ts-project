af584281195e48230e05213aebcf2b2bfed7b69b
lishujing <super_lishujing@163.com>
2022-10-02 10:38:50 +0800

    feat(useEffect): 使用useEffect处理数据监听和API数据获取

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // 直接引入整个 CSS 文件
 // import './App.css'; <div className="app"></div>
 // 使用 JSS 模块化引入组件，将 CSS 文件转换为对象的方式
@@ -17,23 +17,32 @@ interface IState {
 }
 const App: React.FC = (props) => {
   const [count, setCount] = useState<number>(0)
+  const [robotGallery, setRobotGallery] = useState<any>([])
+  useEffect(() => {
+    document.title = `点击${count}次`
+  }, [count])
 
-    return (
-      <div className={styles.app}>
-        <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
-        </div>
-        <button onClick={() => {
-          setCount(count + 1)
-        }}>Click</button>
-        <span>count: {count}</span>
-        <ShoppingCart />
-        {/* <div className={styles.robotList}>
-          {this.state.robotGallery.map(r => <Robot id={r.id} email={r.email} name={r.name} />)}
-        </div> */}
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setRobotGallery(data))
+  }, [])
+  return (
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
       </div>
-    );
+      <button onClick={() => {
+        setCount(count + 1)
+      }}>Click</button>
+      <span>count: {count}</span>
+      <ShoppingCart />
+      <div className={styles.robotList}>
+        {robotGallery.map((r) => <Robot id={r.id} email={r.email} name={r.name} />)}
+      </div>
+    </div>
+  );
 }
 
 export default App;
